ea1c23f9b0b89db12d5d1763587966f0fadd73f8
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-28 10:18:25 +0100

    ready for publish

--- src/qtools/qstr.test.ts
+++ src/qtools/qstr.test.ts
@@ -3,4 +3,8 @@ import { sentencize } from './qstr';
 
 test('sentencize() is correctly implemented', () => {
     expect(sentencize('test')).toBe('Test.');
+    expect(sentencize('test!')).toBe('Test!');
+    expect(sentencize('Test!')).toBe('Test!');
+    expect(sentencize('...')).toBe('...');
+    expect(sentencize('nn')).toBe('Nn.');
 });
